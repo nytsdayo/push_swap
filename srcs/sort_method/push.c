3b6b953c802142b63d9c7df55e3348b854dc9ee5
nytsdayo <nyantsutsu@gmail.com>
2024-09-01 16:50:23 +0900

    add

--- srcs/sort_method/push.c
+++ srcs/sort_method/push.c
@@ -6,27 +6,37 @@
 /*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:53:49 by rnakatan          #+#    #+#             */
-/*   Updated: 2024/09/01 00:45:35 by rnakatan         ###   ########.fr       */
+/*   Updated: 2024/09/01 16:20:53 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void	push(t_list **from, t_list **to)
+void	push(t_stack **src, t_stack **dest)
 {
-	t_list	*tmp;
+	t_stack	*tmp;
 
-	if (!*from)
+	if (!*src)
 		return ;
-	tmp = *from;
-	*from = (*from)->next;
-	tmp->next = *to;
-	*to = tmp;
+	tmp = (*src)->next;
+	(*src)->next = *dest;
+	*dest = *src;
+	*src = tmp;
+}
+{
+	t_list *tmp;
+
+	if (!pair_stack->stack_a.cycle.top)
+		return ;
+	tmp = pair_stack->stack_a.cycle.top;
+	pair_stack->stack_a.cycle.top = pair_stack->stack_a.cycle.top->next;
+	tmp->next = pair_stack->stack_b.cycle.top;
+	pair_stack->stack_b.cycle.top = tmp;
 }
 
 void	pa(t_pair_stack *pair_stack)
 {
-	push(&pair_stack->stack_b.cycle.top, &pair_stack->stack_a.cycle.top);
+	push(&pair_stack);
 	pair_stack->stack_a.count_value++;
 	pair_stack->stack_b.count_value--;
 	write(1, "pa\n", 3);
